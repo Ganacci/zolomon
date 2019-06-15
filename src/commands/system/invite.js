c294bb6118fa103412e3b9b765935012f4c1586e
Ganacci <ganacci@thawnedevelopment.tk>
2019-06-14 21:03:03 -0400

    Dr. Wells has secrets?

--- src/commands/system/invite.js
+++ src/commands/system/invite.js
@@ -19,8 +19,7 @@ module.exports.run = async (client, message, args) => {
 
   message.delete();
   let embed = new Discord.RichEmbed().setColor('BLACK').setFooter('Zolomon', client.user.displayAvatarURL).setAuthor(message.author.username, message.author.displayAvatarURL)
-  .addField('Invite', "Zolomon [here](https://discordapp.com/oauth2/authorize?client_id=577614877468786699&permissions=8&scope=bot)", true)
-  .addField('Support Server', "Our support server is over development company's server, you can join it [here](https://google.com)", true)
+  .addField('Invite', "Invite Zolomon [here](https://discordapp.com/oauth2/authorize?client_id=577614877468786699&permissions=8&scope=bot)", true)
   message.channel.send(embed).then(msg1 => msg1.delete(15000));
   
 
