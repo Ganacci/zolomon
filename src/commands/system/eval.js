70214c55d8d202b9f8202771a958ece4de62ad59
Ganacci <ganacci@thawnedevelopment.tk>
2019-06-15 12:42:04 -0400

    Update eval.js

--- src/commands/system/eval.js
+++ src/commands/system/eval.js
@@ -8,8 +8,14 @@
  */
 
 const Discord = require('discord.js');
-const moment = require("moment");
-require("moment-duration-format");
+
+function clean(text) {
+    if (typeof(text) === "string")
+      return text.replace(/`/g, "`" + String.fromCharCode(8203)).replace(/@/g, "@" + String.fromCharCode(8203));
+    else
+        return text;
+}
+
 
 module.exports.run = async (client, message, args) => {
 
