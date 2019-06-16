f2419eb4f6dce0d1141a987b3ab557b2abac1875
Ganacci <ganacci@thawnedevelopment.tk>
2019-06-16 11:21:43 -0400

    Update statistics.js

--- src/commands/system/statistics.js
+++ src/commands/system/statistics.js
@@ -13,14 +13,12 @@ require("moment-duration-format");
 
 module.exports.run = async (client, message, args) => {
 
-  let instanceofbot = client.guilds.size;
-  let size = client.users.size - instanceofbot;
   let duration = moment.duration(client.uptime).format(" D [days], H [hrs], m [mins], s [secs]");
 
   message.delete();
   let embed = new Discord.RichEmbed().setColor('BLACK').setFooter('Shard #1', client.user.displayAvatarURL).setAuthor(message.author.username, message.author.displayAvatarURL)
   .addField('Guilds', "" + client.guilds.size, true)
-  .addField('Users', "" + size, true)
+  .addField('Users', client.users.size, true)
   .addField('Uptime', duration, true)
   .addField('Memory', `${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)} megabytes`, true)
   .addField('Discord.js', "" + Discord.version, true)
