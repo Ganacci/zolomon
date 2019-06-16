393deb69b699897f87af92a87904543004359ef0
Ganacci <ganacci@thawnedevelopment.tk>
2019-06-16 00:43:45 -0400

    Create serverinfo.js

---
+++ src/commands/information/serverinfo.js
@@ -0,0 +1,38 @@
+/**
+ * 
+ * @project Zolomon
+ * @author Ganacci
+ * @license None
+ * @class command (serverinfo.js)
+ * 
+ */
+
+const Discord = require('discord.js');
+const moment = require("moment");
+require("moment-duration-format");
+
+module.exports.run = async (client, message, args) => {
+
+  let instanceofbot = client.guilds.size;
+  let size = client.users.size - instanceofbot;
+  let duration = moment.duration(client.uptime).format(" D [days], H [hrs], m [mins], s [secs]");
+  let prefix = db.fetch(`prefix_${message.guild.id}`);
+
+  message.delete();
+  let embed = new Discord.RichEmbed().setColor('BLACK').setFooter('Zolomon', client.user.displayAvatarURL).setAuthor(message.guild.name, message.guild.iconURL)
+  .addField('Owner', `<@${message.guild.owner.id}> (${message.guild.owner.id})`, true)
+  .addField('Guild Created', message.guild.createdAt)
+  .addField('Zolomon joined', message.guild.fetchMember(client.user).joinedAt)
+  .addField('Amount of commands executed today', '0 (WiP)')
+  .addField('Server Prefix', prefix)
+  message.channel.send(embed).then(msg1 => msg1.delete(15000));
+
+};
+
+module.exports.help = {
+  name: 'serverinfo',
+  description: 'View the current server information with zolomon',
+  usage: 'serverinfo',
+  category: 'information',
+  accessableby: 'members'
+};
